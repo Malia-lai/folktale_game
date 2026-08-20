01e3c7c2a8648f0d91ba7f997b1d21cd4370b81e
Malia-lai <malia.lar16@gmail.com>
2026-08-20 02:10:55 +0000

    first loop

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -15,74 +15,87 @@ except ImportError:
 # symbols as well.
 SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-print('Caesar Cipher, by Al Sweigart al@inventwithpython.com')
-print('The Caesar cipher encrypts letters by shifting them over by a')
-print('key number. For example, a key of 2 means the letter A is')
-print('encrypted into C, the letter B encrypted into D, and so on.')
-print()
+'''The Caesar cipher encrypts letters by shifting them over by a
+key number. For example, a key of 2 means the letter A is
+encrypted into C, the letter B encrypted into D '''
 
-# Let the user enter if they are encrypting or decrypting:
-while True:  # Keep asking until the user enters e or d.
-    print('Do you want to (e)ncrypt or (d)ecrypt?')
-    response = input('> ').lower()
-    if response.startswith('e'):
-        mode = 'encrypt'
+def caesar():
+    # Let the user enter if they are encrypting or decrypting:
+    while True:  # Keep asking until the user enters e or d.
+        print('Do you want to (e)ncrypt or (d)ecrypt?')
+        response = input('> ').lower()
+        if response.startswith('e'):
+            mode = 'encrypt'
+            break
+        elif response.startswith('d'):
+            mode = 'decrypt'
+            break
+        print('Please enter the letter e or d.')
+
+    # Let the user enter the key to use:
+    while True:  # Keep asking until the user enters a valid key.
+        maxKey = len(SYMBOLS) - 1
+        print('Please enter the key (0 to {}) to use.'.format(maxKey))
+        response = input('> ').upper()
+        if not response.isdecimal():
+            continue
+
+        if 0 <= int(response) < len(SYMBOLS):
+            key = int(response)
+            break
+
+    # Let the user enter the message to encrypt/decrypt:
+    print('Enter the message to {}.'.format(mode))
+    message = input('> ')
+
+    # Caesar cipher only works on uppercase letters:
+    message = message.upper()
+
+    # Stores the encrypted/decrypted form of the message:
+    translated = ''
+
+    # Encrypt/decrypt each symbol in the message:
+    for symbol in message:
+        if symbol in SYMBOLS:
+            # Get the encrypted (or decrypted) number for this symbol.
+            num = SYMBOLS.find(symbol)  # Get the number of the symbol.
+            if mode == 'encrypt':
+                num = num + key
+            elif mode == 'decrypt':
+                num = num - key
+
+            # Handle the wrap-around if num is larger than the length of
+            # SYMBOLS or less than 0:
+            if num >= len(SYMBOLS):
+                num = num - len(SYMBOLS)
+            elif num < 0:
+                num = num + len(SYMBOLS)
+
+            # Add encrypted/decrypted number's symbol to translated:
+            translated = translated + SYMBOLS[num]
+        else:
+            # Just add the symbol without encrypting/decrypting:
+            translated = translated + symbol
+
+    # Display the encrypted/decrypted string to the screen:
+    print(translated)
+
+    try:
+        pyperclip.copy(translated)
+        print('Full {}ed text copied to clipboard.'.format(mode))
+    except:
+        pass  # Do nothing if pyperclip wasn't installed.
+
+while True:
+    print("Would you like to exit or to open the caesar engine ?")
+    print("'exit' or 'open': ")
+    reponse = input().lower()
+    if reponse == "exit":
+        print("Going back to the main room")
         break
-    elif response.startswith('d'):
-        mode = 'decrypt'
+    elif reponse == "open":
+        caesar()
+        print("going back to the main room")
         break
-    print('Please enter the letter e or d.')
-
-# Let the user enter the key to use:
-while True:  # Keep asking until the user enters a valid key.
-    maxKey = len(SYMBOLS) - 1
-    print('Please enter the key (0 to {}) to use.'.format(maxKey))
-    response = input('> ').upper()
-    if not response.isdecimal():
-        continue
-
-    if 0 <= int(response) < len(SYMBOLS):
-        key = int(response)
-        break
-
-# Let the user enter the message to encrypt/decrypt:
-print('Enter the message to {}.'.format(mode))
-message = input('> ')
-
-# Caesar cipher only works on uppercase letters:
-message = message.upper()
-
-# Stores the encrypted/decrypted form of the message:
-translated = ''
-
-# Encrypt/decrypt each symbol in the message:
-for symbol in message:
-    if symbol in SYMBOLS:
-        # Get the encrypted (or decrypted) number for this symbol.
-        num = SYMBOLS.find(symbol)  # Get the number of the symbol.
-        if mode == 'encrypt':
-            num = num + key
-        elif mode == 'decrypt':
-            num = num - key
-
-        # Handle the wrap-around if num is larger than the length of
-        # SYMBOLS or less than 0:
-        if num >= len(SYMBOLS):
-            num = num - len(SYMBOLS)
-        elif num < 0:
-            num = num + len(SYMBOLS)
-
-        # Add encrypted/decrypted number's symbol to translated:
-        translated = translated + SYMBOLS[num]
     else:
-        # Just add the symbol without encrypting/decrypting:
-        translated = translated + symbol
-
-# Display the encrypted/decrypted string to the screen:
-print(translated)
-
-try:
-    pyperclip.copy(translated)
-    print('Full {}ed text copied to clipboard.'.format(mode))
-except:
-    pass  # Do nothing if pyperclip wasn't installed.
+        print("invalid key, make sure to type 'exit' or 'open'")
--- game.py
+++ game.py
@@ -1,4 +1,45 @@
-greeting = "hello world! from folktale"
+
+#Variable declaration 
+game_running = False
+menu_opened = False
+opened_file = False
+key_found = False
+
+
+
+def main_room():
+    print('''describe main room and acessible keys
+            'menu' to open the menu 
+            'left' to go to the bedroom
+            'right' to go to the kitchen
+            'front' to knock on the princess' room ''')
+    reponses = input().lower()
+    if reponses == 'menu':
+        print("Menu opening...")
+
+
 
 print("")
-print(greeting)
+print('''Welcome dear user unto 'the art of a hero'! \n
+    You are the hero, your role is to rescue the princess who's stuck in her room,
+    or at least that's what everybody says... \n
+    will you acheive this ? \n
+    choose the options below: 
+    'exit' -> quit the game
+    'enter' -> start the game
+    'menu' -> open the menu
+    ''')
+reponse = input().lower()
+if reponse == 'exit':
+    print('I guess someone else will acheive this task...')
+    pass
+elif reponse == 'enter':
+    print("We have a real hero! \n")
+    game_running = True
+    main_room()
+elif reponse == 'menu':
+    print("menu opening...")    
+else:
+    raise print("make sure to type 'exit' 'enter' or 'menu'") 
+
+
